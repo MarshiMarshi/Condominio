f86118eec7a4aa2b3f207f0abff48aa610b6c6b0
vitorhcl <vitorhcl00@gmail.com>
2022-11-22 16:57:50 -0300

    Adicionando BlocoEnum, Morador e IMoradorRepository

--- Projeto_DS_Condominio/Model/IMoradorRepository.cs
+++ Projeto_DS_Condominio/Model/IMoradorRepository.cs
@@ -8,5 +8,10 @@ namespace Projeto_DS_Condominio.Model
 {
     public interface IMoradorRepository
     {
+        void Adicionar(Morador morador);
+        void Editar(Morador morador);
+        void Delete(int id);
+        IEnumerable<Encomenda> GetAll();
+        IEnumerable<Encomenda> GetByStatus();
     }
 }
--- Projeto_DS_Condominio/Model/Morador.cs
+++ Projeto_DS_Condominio/Model/Morador.cs
@@ -3,11 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Projeto_DS_Condominio.Enums;
 
 namespace Projeto_DS_Condominio.Model
 {
     public class Morador
     {
+        private int _id;
+        private string _nome;
+        private string _rg;
+        private string _cpf;
+        private BlocoEnum _bloco;
+        private int _apartamento;
 
+        public int Id { get => _id; set => _id = value; }
+        public string Nome { get => _nome; set => _nome = value; }
+        public string Rg { get => _rg; set => _rg = value; }
+        public string Cpf { get => _cpf; set => _cpf = value; }
+        public BlocoEnum Bloco { get => _bloco; set => _bloco = value; }
+        public int Apartamento { get => _apartamento; set => _apartamento = value; }
     }
 }
