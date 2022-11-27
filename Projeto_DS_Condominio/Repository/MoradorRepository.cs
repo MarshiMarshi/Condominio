fcbb00a673987508289a1368dadad1e052a43a11
vitorhcl <vitorhcl00@gmail.com>
2022-11-27 12:58:03 -0300

    Implementando GetAll() no MoradorRepository()

--- Projeto_DS_Condominio/Repository/MoradorRepository.cs
+++ Projeto_DS_Condominio/Repository/MoradorRepository.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Projeto_DS_Condominio.Enums;
 using Projeto_DS_Condominio.Model;
 
 namespace Projeto_DS_Condominio.Repository
@@ -29,14 +31,35 @@ namespace Projeto_DS_Condominio.Repository
             throw new NotImplementedException();
         }
 
-        public IEnumerable<Encomenda> GetAll()
+        public IEnumerable<Morador> GetAll()
         {
-            throw new NotImplementedException();
-        }
+            List<Morador> moradorList = new List<Morador>();
+            using (var connection = new SqlConnection(connectionString))
+            using (var command = new SqlCommand())
+            {
+                connection.Open();
+                command.Connection = connection;
+                command.CommandText = "select * from Morador order by bloco_morador, apartamento desc";
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Morador morador = new Morador();
+                        morador.Id = (int)reader[0];
+                        morador.Nome = reader[1].ToString();
+                        morador.Rg = reader[2].ToString();
+                        morador.Cpf = reader[3].ToString();
 
-        public IEnumerable<Encomenda> GetByStatus()
-        {
-            throw new NotImplementedException();
+                        BlocoEnum bloco;
+                        Enum.TryParse<BlocoEnum>(reader[4].ToString(), out bloco);
+                        morador.Bloco = bloco;
+
+                        morador.Apartamento = Int32.Parse(reader[5].ToString());
+                        moradorList.Add(morador);
+                    }
+                }
+            }
+            return moradorList;
         }
     }
 }
