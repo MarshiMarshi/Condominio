2f89cf83dffb710f893589c1436f11518ce7fe5c
vitorhcl <vitorhcl00@gmail.com>
2022-11-29 23:21:02 -0300

    Implementando a interface IEncomendaRepository na classe EncomendaRepository

--- Projeto_DS_Condominio/Repository/EncomendaRepository.cs
+++ Projeto_DS_Condominio/Repository/EncomendaRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Projeto_DS_Condominio.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,11 +7,31 @@ using System.Threading.Tasks;
 
 namespace Projeto_DS_Condominio.Repository
 {
-    public class EncomendaRepository : RepositoryBase
+    public class EncomendaRepository : RepositoryBase, IEncomendaRepository
     {
         public EncomendaRepository(string connectionString)
         {
             this.connectionString = connectionString;
+        }
+
+        public void Adicionar(Encomenda encomenda)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Editar(Encomenda encomenda)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Encomenda> GetAll()
+        {
+            throw new NotImplementedException();
         }
     }
 }
